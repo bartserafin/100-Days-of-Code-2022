66f703ab47430a4a395388bcd0325f51433ce52b
bartserafin <86855505+bartserafin@users.noreply.github.com>
2022-02-10 16:01:27 +0100

    Delete config.py

--- 35.Day_35_SMS_Rain_Alert/config.py
+++
@@ -1,6 +0,0 @@
-API_KEY = 'e3d97a86df9931b1754ae06454d782a0'
-
-# twilio data
-account_sid = 'AC496ebc1c379819f9845bf0acadced4fe'
-auth_token = '48a2d8a59b10ce5334c755ada5dd5618'
-my_number = '+48791586430'
